0cce7e9dce1e7308e81064fa764db22745e90a35
Corneliu Olaru <cornel.olaru47@gmail.com>
2024-07-19 18:11:45 +0300

    resolved issue with db URI undefined

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,19 @@
-require('dotenv').config();
+require('dotenv').config(); // Asigura-te că variabilele de mediu sunt încărcate la început
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DB_URL, {
+// Suprimă avertizarea deprecației strictQuery
+mongoose.set('strictQuery', true);
+
+const dbURL = process.env.DB_URL;
+console.log('DB_URL:', dbURL); // Verifică dacă DB_URL este definită
+
+if (!dbURL) {
+  console.error('DB_URL not defined in environment variables');
+  process.exit(1);
+}
+
+mongoose.connect(dbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -12,6 +24,7 @@ database.on('error', (error) => {
   console.error('Connection error:', error);
   process.exit(1);
 });
+
 database.once('open', function () {
   console.log('Connected to MongoDB');
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config();
 const express = require('express');
 const database = require('./config/database');
 const eventRoutes = require('./routes/eventRoutes');
@@ -6,7 +8,8 @@ const app = express();
 app.use(express.json());
 app.use('/events', eventRoutes);
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
